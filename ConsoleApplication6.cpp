4efb79cdc7fd752862cafc09c8ae8e9bfe52579f
lylliyan <103178177+lylliyan@users.noreply.github.com>
2022-04-07 19:07:58 +0800

    Add files via upload

---
+++ ConsoleApplication6.cpp
@@ -0,0 +1,14 @@
+﻿#include"Antique.h"
+#include<iostream>
+#include<string>
+using namespace std;
+void main()
+{
+	Antique a1, a2;
+	cout << "莲鹤方壶体积：" << a1.Volume(30.5, 54.0, 126.5) << "立方厘米" << endl;
+	cout << "贾湖骨笛：" << a2.Volume(2.0, 23.1) << "立方厘米" << endl;
+	system("pause");
+}
+
+
+
